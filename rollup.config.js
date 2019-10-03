980f0955618c3584af92d6f1d608ce30418acb2b
Marcin Krawczyk <marckraw@icloud.com>
2019-10-03 17:25:30 +0200

    [upd] better multiple outputs

--- rollup.config.js
+++ rollup.config.js
@@ -18,42 +18,28 @@ const plugins = {
 export default [
     {
         input: "src/scripts/app.js",
-        output: {
-            file: "dist/scripts/app.iife.js",
-            format: "iife"
-        },
-        ...plugins
-    },
-    {
-        input: "src/scripts/app.js",
-        output: {
-            file: "dist/scripts/app.system.js",
-            format: "system"
-        },
-        ...plugins
-    },
-    {
-        input: "src/scripts/app.js",
-        output: {
-            file: "dist/scripts/app.cjs.js",
-            format: "cjs"
-        },
-        ...plugins
-    },
-    {
-        input: "src/scripts/app.js",
-        output: {
-            file: "dist/scripts/app.umd.js",
-            format: "umd"
-        },
-        ...plugins
-    },
-    {
-        input: "src/scripts/app.js",
-        output: {
-            file: "dist/scripts/app.esm.js",
-            format: "esm"
-        },
+        output: [
+            {
+                file: "dist/scripts/app.iife.js",
+                format: "iife"
+            },
+            {
+                file: "dist/scripts/app.system.js",
+                format: "system"
+            },
+            {
+                file: "dist/scripts/app.cjs.js",
+                format: "cjs"
+            },
+            {
+                file: "dist/scripts/app.umd.js",
+                format: "umd"
+            },
+            {
+                file: "dist/scripts/app.esm.js",
+                format: "esm"
+            }
+        ],
         ...plugins
     }
 ];
