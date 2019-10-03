7faed2e05db00f1823893df193b30bdb8218c30d
Marcin Krawczyk <marckraw@icloud.com>
2019-10-03 17:22:09 +0200

    [add] naive add output targets with different systems by copy pasting...

--- rollup.config.js
+++ rollup.config.js
@@ -6,16 +6,70 @@ rimraf.sync("dist");
 const pkg = require("./package.json");
 const { version } = pkg;
 
-export default {
-    input: "src/scripts/app.js",
-    output: {
-        file: "dist/scripts/app.js",
-        format: "iife"
+export default [
+    {
+        input: "src/scripts/app.js",
+        output: {
+            file: "dist/scripts/app.iife.js",
+            format: "iife"
+        },
+        plugins: [
+            replace({
+                delimiters: ["version{{", "}}"],
+                version
+            })
+        ]
     },
-    plugins: [
-        replace({
-            delimiters: ["version{{", "}}"],
-            version
-        })
-    ]
-};
+    {
+        input: "src/scripts/app.js",
+        output: {
+            file: "dist/scripts/app.system.js",
+            format: "system"
+        },
+        plugins: [
+            replace({
+                delimiters: ["version{{", "}}"],
+                version
+            })
+        ]
+    },
+    {
+        input: "src/scripts/app.js",
+        output: {
+            file: "dist/scripts/app.cjs.js",
+            format: "cjs"
+        },
+        plugins: [
+            replace({
+                delimiters: ["version{{", "}}"],
+                version
+            })
+        ]
+    },
+    {
+        input: "src/scripts/app.js",
+        output: {
+            file: "dist/scripts/app.umd.js",
+            format: "umd"
+        },
+        plugins: [
+            replace({
+                delimiters: ["version{{", "}}"],
+                version
+            })
+        ]
+    },
+    {
+        input: "src/scripts/app.js",
+        output: {
+            file: "dist/scripts/app.esm.js",
+            format: "esm"
+        },
+        plugins: [
+            replace({
+                delimiters: ["version{{", "}}"],
+                version
+            })
+        ]
+    }
+];
